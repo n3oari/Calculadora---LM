82cd191326b5abede5c5a8d6f3613155421af6bf
Javier Andrés Caballero <javierspam555@gmail.com>
2025-01-23 23:09:23 +0100

    Calculadora plantilla  - Funciones -> operaciones aritmeticas basicas

--- eventos.js
+++ eventos.js
@@ -9,42 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     .addEventListener("click", function (click) {
       num1 = this.inner.text;
       document.getElementById("num1").value = num1;
-      /*
-  document.addEventListener("click", function (click) {
-    switch (click.target.value) {
-      case "1":
-        num1.value == "1";
-        break;
-      case 2:
-        num1.value += "2";
-        break;
-      case 3:
-        num1.value += "3";
-        break;
-      case 4:
-        num1.value += "4";
-        break;
-      case 5:
-        num1.value += "5";
-        break;
-      case 6:
-        num1.value += "6";
-        break;
-      case 7:
-        num1.value += "7";
-        break;
-      case 8:
-        num1.value += "8";
-        break;
-      case 9:
-        num1.value += "9";
-        break;
-      case 0:
-        num1.value += "0";
-        break;
-    }
-  });
-*/
+
       var suma = document.getElementById("sumar");
       var resta = document.getElementById("resta");
       var multiplicacion = document.getElementById("multiplicacion");
