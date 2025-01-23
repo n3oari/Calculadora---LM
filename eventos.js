47930127ceff70b5da2fd98aa072741201bd7e71
Javier Andrés Caballero <javierspam555@gmail.com>
2025-01-23 23:49:04 +0100

    Pequeño error corregido

--- eventos.js
+++ eventos.js
@@ -2,64 +2,47 @@ document.addEventListener("DOMContentLoaded", function () {
   var num1 = document.getElementById("num1");
   var num2 = document.getElementById("num2");
 
-  var numB = document.getElementsByClassName(numB);
+  var suma = document.getElementById("sumar");
+  var resta = document.getElementById("resta");
+  var multiplicacion = document.getElementById("multiplicacion");
+  var division = document.getElementById("division");
 
-  document
-    .getElementsByClassName("numB")
-    .addEventListener("click", function (click) {
-      num1 = this.inner.text;
-      document.getElementById("num1").value = num1;
+  suma.addEventListener("click", function (e) {
+    console.log("Suma");
 
-      var suma = document.getElementById("sumar");
-      var resta = document.getElementById("resta");
-      var multiplicacion = document.getElementById("multiplicacion");
-      var division = document.getElementById("division");
+    if (num1.value == "" || num2.value == "") {
+      alert("No se puede realizar una operacion con campos vacios");
+      return;
+    }
+    alert("RESULTADO -> " + (parseFloat(num1.value) + parseFloat(num2.value)));
+  });
 
-      suma.addEventListener("click", function (e) {
-        console.log("Suma");
+  resta.addEventListener("click", function (e) {
+    console.log("Resta");
 
-        if (num1.value == "" || num2.value == "") {
-          alert("No se puede realizar una operacion con campos vacios");
-          return;
-        }
-        alert(
-          "RESULTADO -> " + (parseFloat(num1.value) + parseFloat(num2.value))
-        );
-      });
+    if (num1.value == "" || num2.value == "") {
+      alert("No se puede realizar una operacion con campos vacios");
+      return;
+    }
+    alert("RESULTADO -> " + (parseFloat(num1.value) - parseFloat(num2.value)));
+  });
 
-      resta.addEventListener("click", function (e) {
-        console.log("Resta");
+  multiplicacion.addEventListener("click", function (e) {
+    console.log("Multiplicacion");
 
-        if (num1.value == "" || num2.value == "") {
-          alert("No se puede realizar una operacion con campos vacios");
-          return;
-        }
-        alert(
-          "RESULTADO -> " + (parseFloat(num1.value) - parseFloat(num2.value))
-        );
-      });
+    if (num1.value == "" || num2.value == "") {
+      alert("No se puede realizar una operacion con campos vacios");
+      return;
+    }
+    alert("RESULTADO -> " + parseFloat(num1.value) * parseFloat(num2.value));
+  });
 
-      multiplicacion.addEventListener("click", function (e) {
-        console.log("Multiplicacion");
-
-        if (num1.value == "" || num2.value == "") {
-          alert("No se puede realizar una operacion con campos vacios");
-          return;
-        }
-        alert(
-          "RESULTADO -> " + parseFloat(num1.value) * parseFloat(num2.value)
-        );
-      });
-
-      division.addEventListener("click", function (e) {
-        console.log("Division");
-        if (num1.value == "" || num2.value == "") {
-          alert("No se puede realizar una operacion con campos vacios");
-          return;
-        }
-        alert(
-          "RESULTADO -> " + parseFloat(num1.value) / parseFloat(num2.value)
-        );
-      });
-    });
+  division.addEventListener("click", function (e) {
+    console.log("Division");
+    if (num1.value == "" || num2.value == "") {
+      alert("No se puede realizar una operacion con campos vacios");
+      return;
+    }
+    alert("RESULTADO -> " + parseFloat(num1.value) / parseFloat(num2.value));
+  });
 });
